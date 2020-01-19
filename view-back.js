fc6972c7d5779d4fac77654e549348860c2472cd
dlzhao <15232113979@163.com>
2020-01-19 11:35:50 +0800

    Update view-back.js

--- view-back.js
+++ view-back.js
@@ -2,7 +2,6 @@
 
 // 解决ios12上软键盘顶起页面后隐藏不回弹的问题
 export default function viewBack() {
-    console.log('viewback1');
     var ua = navigator.userAgent;
     var weiboInfo = ua.match(/weibo__([\d\.]+)/i);
     var weiboVersion = weiboInfo && weiboInfo[1] ? weiboInfo[1] : 0;
@@ -15,8 +14,7 @@ export default function viewBack() {
     var currentPosition, timer;
     var speed = 1; // 页面滚动距离
 
-    if (weiboVersion > 8.11 && IosVersion === 12) {   //ios12，且微博版本高于8.11
-        console.log('viewbac2');
+    if (weiboVersion > 8.11 && IosVersion >= 12) {   //ios12，且微博版本高于8.11
         timer = setInterval(function(){
             currentPosition = document.documentElement.scrollTop || document.body.scrollTop;
             currentPosition -= speed; 
@@ -24,7 +22,6 @@ export default function viewBack() {
             currentPosition += speed; //speed变量
             window.scrollTo(0, currentPosition); // 页面向下滚动
             clearInterval(timer);
-            console.log('viewbac3');
         },1);
     }
 }
