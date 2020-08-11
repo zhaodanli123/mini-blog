00dad9ea91d6269c1c734c6c824e4b7ede50f3d7
zhaodanli123 <15232113979@163.com>
2020-08-11 22:19:13 +0800

    vue directive/extend/observabel

---
+++ vue-extend/loading/loading.vue
@@ -0,0 +1,23 @@
+<template>
+  <transition name="custom-loading-fade">
+    <!--loading蒙版-->
+    <div v-show="visible" class="custom-loading-mask">
+      <!--loading中间的图标-->
+      <div class="custom-loading-spinner">
+        <i class="custom-spinner-icon"></i>
+        <!--loading上面显示的文字-->
+        <p class="custom-loading-text">{{ text }}</p>
+      </div>
+    </div>
+  </transition>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      text: '',
+      visible: false
+    }
+  }
+}
+</script>
---
+++ vue-observable/component.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    {{ userInfo.name }}
+  </div>
+</template>
+<script>
+import { store, mutations } from './store'
+export default {
+  computed: {
+    userInfo() {
+      return store.userInfo
+    }
+  },
+  created() {
+    mutations.setUserInfo({
+      name: '子君'
+    })
+  }
+}
+</script>
